c019b0933793eb87c571644917e98a52d3716e93
Felipe Acosta <felipegonac0@gmail.com>
2024-09-14 18:59:13 -0600

    feat: added change status endpoint

--- src/fingo-handle-personal-plan-status-service/src/lambda_function.py
+++ src/fingo-handle-personal-plan-status-service/src/lambda_function.py
@@ -1,7 +1,17 @@
 import json
+from db.client import DynamoDBClient
+from config.env import DYNAMODB_TABLE_NAME
+import uuid
+
+db_client = DynamoDBClient(DYNAMODB_TABLE_NAME)
 
 def lambda_handler(event, context):
+    response = db_client.complete_node({
+        'user_id': event["user_id"],
+        'path_node': event["node_id"]
+    })
+
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'statusCode': 200 if response['status'] == 'success' else 500,
+        'body': json.dumps(response)
     }
