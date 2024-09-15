56f75f8d7dc536b9fc4c26876e37830609c1ec05
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-09-14 23:07:46 -0600

    feat: deploy consul personal plans lambda

--- src/fingo-consult-personal-plans-service/src/lambda_function.py
+++ src/fingo-consult-personal-plans-service/src/lambda_function.py
@@ -2,9 +2,6 @@ import json
 
 def lambda_handler(event, context):
     """AWS Lambda Function to handle requests to the Personal Plans Service"""
-    
-
-
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
