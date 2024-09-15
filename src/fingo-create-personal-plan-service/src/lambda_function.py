b239426be40aa85c1065e91c9782e000dfc2861e
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-09-14 18:22:01 -0600

    feat: include user id

--- src/fingo-create-personal-plan-service/src/lambda_function.py
+++ src/fingo-create-personal-plan-service/src/lambda_function.py
@@ -5,7 +5,10 @@ from config.env import DYNAMODB_TABLE_NAME
 dynamodb_client = DynamoDBClient(DYNAMODB_TABLE_NAME)
 
 def lambda_handler(event, context):
-    response = dynamodb_client.insert_item(event)
+    response = dynamodb_client.insert_item({
+        'user_id': event['user_id'],
+        'event': event
+    })
 
     return {
         'statusCode': 200 if response['status'] == 'success' else 500,
