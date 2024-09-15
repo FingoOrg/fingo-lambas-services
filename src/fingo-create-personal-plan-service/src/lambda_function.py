223a1619d1266ce2f64ab4f646352283e6c45fae
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-09-14 18:24:54 -0600

    feat: include uuid

--- src/fingo-create-personal-plan-service/src/lambda_function.py
+++ src/fingo-create-personal-plan-service/src/lambda_function.py
@@ -1,12 +1,13 @@
 import json
 from dynamodb.client import DynamoDBClient
 from config.env import DYNAMODB_TABLE_NAME
+import uuid
 
 dynamodb_client = DynamoDBClient(DYNAMODB_TABLE_NAME)
 
 def lambda_handler(event, context):
     response = dynamodb_client.insert_item({
-        'user_id': event['user_id'],
+        'user_id': uuid.uuid4().hex,
         'event': event
     })
 
