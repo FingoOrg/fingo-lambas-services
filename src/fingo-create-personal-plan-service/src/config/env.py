e06f1f84607221e0ea9321711bdb645401696559
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-09-14 19:07:28 -0600

    feat: include bedrock client

--- src/fingo-create-personal-plan-service/src/config/env.py
+++ src/fingo-create-personal-plan-service/src/config/env.py
@@ -1,3 +1,5 @@
 import os
 
 DYNAMODB_TABLE_NAME = os.environ['TABLE_NAME']
+BEDROCK_MODEL_ID = os.environ['BEDROCK_MODEL_ID']
+AWS_REGION = os.environ['AWS_REGION']
--- src/fingo-create-personal-plan-service/src/lambda_function.py
+++ src/fingo-create-personal-plan-service/src/lambda_function.py
@@ -1,13 +1,33 @@
 import json
 from dynamodb.client import DynamoDBClient
-from config.env import DYNAMODB_TABLE_NAME
+from bedrock.client import BedrockClient
+from config.env import (
+    DYNAMODB_TABLE_NAME,
+    AWS_REGION,
+    BEDROCK_MODEL_ID,
+)
 import uuid
 
 dynamodb_client = DynamoDBClient(DYNAMODB_TABLE_NAME)
 
 def lambda_handler(event, context):
     """AWS Lambda handler."""
-    response = dynamodb_client.insert_item(event)
+    # response = dynamodb_client.insert_item(event)
+
+    llm_query = event['llm_query']
+    bedrock_client = BedrockClient(
+        region_name=AWS_REGION,
+        max_tokens=300,  
+        temperature=0.7,  
+        top_p=0.9,  
+        top_k=50,
+        stop_sequences=["\n"]
+    )
+
+    response = bedrock_client.invoke_anthropic_claude(
+        BEDROCK_MODEL_ID,
+        llm_query,
+    )
 
     return {
         'statusCode': 200 if response['status'] == 'success' else 500,
