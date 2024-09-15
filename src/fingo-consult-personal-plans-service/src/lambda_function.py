8c6acc6fcc85d9eba809fa73a534c88d75a9e090
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-09-15 01:29:48 -0600

    feat: restore schema

--- src/fingo-consult-personal-plans-service/src/lambda_function.py
+++ src/fingo-consult-personal-plans-service/src/lambda_function.py
@@ -8,13 +8,22 @@ def decimal_default(obj):
         return float(obj)
     raise TypeError
 
+def sum_completed_amounts(response):
+    total = 0
+    for plan in response['data'][0]['bedrock_response']:
+        if plan['status'] == True:
+            total += plan['amount']
+    return total
+
 dynamodb_client = DynamoDBClient(os.environ['DYNAMODB_TABLE_NAME'])
 
 def lambda_handler(event, context):
     """AWS Lambda Function to handle requests to the Personal Plans Service"""
     response = dynamodb_client.scan_item_by_user_id(os.environ['USER_ID'])
+    completed_amount = sum_completed_amounts(response)
 
     return {
         'statusCode': 200,
-        'body': json.dumps(response, default=decimal_default)
+        'body': json.dumps(response, default=decimal_default),
+        'completed_amount': completed_amount
     }
