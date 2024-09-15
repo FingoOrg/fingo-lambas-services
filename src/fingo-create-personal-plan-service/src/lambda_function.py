cde99f64dad3654e91859f6d60cd4aab85791ba8
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-09-14 19:25:58 -0600

    feat: include dynamodb - bedrock client

--- src/fingo-create-personal-plan-service/src/lambda_function.py
+++ src/fingo-create-personal-plan-service/src/lambda_function.py
@@ -5,29 +5,27 @@ from config.env import (
     DYNAMODB_TABLE_NAME,
     BEDROCK_MODEL_ID,
 )
-import uuid
 
 dynamodb_client = DynamoDBClient(DYNAMODB_TABLE_NAME)
 
 def lambda_handler(event, context):
     """AWS Lambda handler."""
-    # response = dynamodb_client.insert_item(event)
-
     llm_query = event['llm_query']
     bedrock_client = BedrockClient(
-        region_name='us-west-2',
-        max_tokens=300,  
-        temperature=0.7,  
-        top_p=0.9,  
-        top_k=50,
+        region_name='us-west-2', max_tokens=300,  
+        temperature=0.7, top_p=0.9, top_k=50,
     )
 
-    response = bedrock_client.invoke_anthropic_claude(
-        BEDROCK_MODEL_ID,
-        llm_query,
+    bedrock_response = bedrock_client.invoke_anthropic_claude(BEDROCK_MODEL_ID, llm_query,)
+
+    dynamo_response = dynamodb_client.insert_item(
+        llm_query, event['form_data'], bedrock_response,
     )
 
     return {
         'statusCode': 200,
-        'body': json.dumps(response)
+        'body': json.dumps({
+            'bedrock_response': bedrock_response,
+            'dynamo_response': dynamo_response,
+        }),
     }
