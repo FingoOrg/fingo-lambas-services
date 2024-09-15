70d8cd14d977b235dc498813ba6844ac1fe9f299
Felipe Acosta <felipegonac0@gmail.com>
2024-09-15 00:23:50 -0600

    feat: testing new schema

--- src/fingo-handle-personal-plan-status-service/src/db/client.py
+++ src/fingo-handle-personal-plan-status-service/src/db/client.py
@@ -11,7 +11,7 @@ class DynamoDBClient:
         try:
 
             response = self.table.update_item(
-                Key={"user_id": item['user_id'], "path_id": item['path_id']},
+                Key={"path_id": item['path_id'], "user_id": item['user_id']},
                 UpdateExpression="set bedrock_response=:br, badge=:bg",
                 ExpressionAttributeValues={":br": item['bedrock_response'], ":bg": item['badge']},
                 ReturnValues="UPDATED_NEW",
